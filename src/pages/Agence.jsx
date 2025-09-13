3ff35c00b1e0530107fdd51c8db11da8768de23c
Bhuvan Bhattarai <bhuvanbhattarai07@gmail.com>
2025-09-14 00:27:49 +0545

    finaliz

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -1,7 +1,9 @@
+import React, { useRef } from 'react'
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/all'
-import { useRef } from 'react'
+import Footer from '../components/common/footer'
+
 
 const Agence = () => {
 
@@ -11,30 +13,30 @@ const Agence = () => {
     const imageRef = useRef(null)
 
     const imageArray = [
-        'https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg',
-        'https://k72.ca/uploads/teamMembers/Olivier_480x640-480x640.jpg',
-        'https://k72.ca/uploads/teamMembers/Lawrence_480x640-480x640.jpg',
-        'https://k72.ca/uploads/teamMembers/HugoJoseph_480x640-480x640.jpg',
-        'https://k72.ca/uploads/teamMembers/ChantalG_480x640-480x640.jpg',
-        'https://k72.ca/uploads/teamMembers/MyleneS_480x640-480x640.jpg',
-        'https://k72.ca/uploads/teamMembers/SophieA_480x640-480x640.jpg',
-        'https://k72.ca/uploads/teamMembers/Claire_480x640-480x640.jpg',
-        'https://k72.ca/uploads/teamMembers/Michele_480X640-480x640.jpg',
-        'https://k72.ca/uploads/teamMembers/MEL_480X640-480x640.jpg',
-        'https://k72.ca/uploads/teamMembers/CAMILLE_480X640_2-480x640.jpg',
-        'https://k72.ca/uploads/teamMembers/MAXIME_480X640_2-480x640.jpg',
-        'https://k72.ca/uploads/teamMembers/MEGGIE_480X640_2-480x640.jpg',
-        'https://k72.ca/uploads/teamMembers/joel_480X640_3-480x640.jpg',
+        '../../public/agenceimg/img1.jpg',
+        '../../public/agenceimg/img2.jpg',
+        '../../public/agenceimg/img3.jpg',
+        '../../public/agenceimg/img4.jpg',
+        '../../public/agenceimg/img5.jpg',
+        '../../public/agenceimg/img6.jpg',
+        '../../public/agenceimg/img7.jpg',
+        '../../public/agenceimg/img8.jpg',
+        '../../public/agenceimg/img9.jpg',
+        '../../public/agenceimg/img10.jpg',
+        '../../public/agenceimg/img11.jpg',
+        '../../public/agenceimg/img12.jpg',
+        '../../public/agenceimg/img13.jpg',
+        '../../public/agenceimg/img14.jpg'
+
     ]
 
     useGSAP(function () {
-
         gsap.to(imageDivRef.current, {
             scrollTrigger: {
                 trigger: imageDivRef.current,
-                // markers: true,
-                start: 'top 28%',
-                end: 'top -70%',
+                markers: false,
+                start: 'top -15%',
+                end: 'top -250%',
                 pin: true,
                 pinSpacing: true,
                 pinReparent: true,
@@ -42,7 +44,11 @@ const Agence = () => {
                 scrub: 1, // smooth scrubbing with 1s easing
                 anticipatePin: 1,
                 invalidateOnRefresh: true,
+
+
                 onUpdate: (elem) => {
+                    // console.log(Math.floor(elem.progress * imageArray.length))
+                    // const imageIndex = Math.floor(elem.progress * imageArray.length)
                     let imageIndex;
                     if (elem.progress < 1) {
                         imageIndex = Math.floor(elem.progress * imageArray.length)
@@ -52,30 +58,50 @@ const Agence = () => {
                     imageRef.current.src = imageArray[imageIndex]
                 }
             }
+
         })
     })
-
-
     return (
-        <div className='parent text-black'>
-            <div id='page1' className='py-1 '>
-                <div ref={imageDivRef} className='absolute overflow-hidden lg:h-[20vw] h-[30vw] lg:rounded-3xl rounded-xl lg:w-[15vw] w-[25vw] lg:top-96 -top-80 lg:left-[30vw] left-[30vw]'>
-                    <img ref={imageRef} className='h-full object-cover w-full' src="https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg" alt="" />
+        <>
+            <div className='section1 text-black'>
+                <div ref={imageDivRef} className='z-[-1] absolute h-[150px] lg:h-[20vw] rounded-3xl w-[120px] lg:w-[14vw] sm:top-20 lg:top-0 top-10 sm:left-[30vw] left-[10vw] overflow-hidden'>
+                    <img ref={imageRef} className=' h-full w-full object-cover' src="../../public/agenceimg/img1.jpg" alt="img 1" />
                 </div>
-                <div className='relative font-[font2]'>
-                    <div className='lg:mt-[55vh] mt-[30vh]'>
-                        <h1 className='text-[20vw] text-center uppercase leading-[18vw]'>Soixan7e <br />
-                            Douze</h1>
-                    </div>
-                    <div className='lg:pl-[40%] lg:mt-20 mt-4 p-3'>
-                        <p className='lg:text-6xl text-xl leading-tight'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Notre curiosité nourrit notre créativité. On reste humbles et on dit non aux gros egos, même le vôtre. Une marque est vivante. Elle a des valeurs, une personnalité, une histoire. Si on oublie ça, on peut faire de bons chiffres à court terme, mais on la tue à long terme. C’est pour ça qu’on s’engage à donner de la perspective, pour bâtir des marques influentes.</p>
+                <div className='relative font-[font2] !z-40'>
+                    <div className='mt-[55vh]'>
+                        <h1 className='text-5xl sm:text-[20vw] text-center uppercase leading-[18vw]'>Soixan7e <br /> Douze</h1>
+                        <div className='pl-[30%] sm:pl-[40%] mt-20 p-3'>
+                            <p className='text-xl sm:text-6xl lg:text-[3.5rem] leading-tight'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Notre curiosité nourrit notre créativité. On reste humbles et on dit non aux gros egos, même le vôtre. Une marque est vivante. Elle a des valeurs, une personnalité, une histoire. Si on oublie ça, on peut faire de bons chiffres à court terme, mais on la tue à long terme. C’est pour ça qu’on s’engage à donner de la perspective, pour bâtir des marques influentes.</p>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div id='page2' className=" h-screen">
+            <div className='section2 px-10 lg:px-20  sm:px-32 py-20 !z-40'>
+                <div className="top flex sm:flex-row flex-col justify-start items-start gap-10 sm:gap-72 ">
+                    <div className="box text-2xl font-semibold">Expertise</div>
+                    <div className="box">
+                        <ul class="list-none text-2xl font-semibold sm:ml-0 ml-10">
+                            <li>Stratégie</li>
+                            <li>Publicité</li>
+                            <li>Branding</li>
+                            <li>Design</li>
+                            <li>Contenu</li>
+                        </ul>
+
+                    </div>
+                </div>
+                <div className='sm:mt-0 mt-32  text-[0.8rem] lg:text-[1rem] sm:text-[1.2rem] font-semibold flex sm:flex-row flex-col justify-center items-start py-28 gap-5 !z-50'>
+                    <div>Nos projets_ naissent dans l’humilité, grandissent dans la curiosité et vivent grâce à la créativité sous toutes ses formes.</div>
+                    <div>Notre création_ bouillonne dans un environnement où le talent a le goût d’exploser. Où on se sent libre d’être la meilleure version de soi-même.</div>
+                    <div>Notre culture_ c’est l’ouverture aux autres. Point. Tout l’équipage participe à bâtir une agence dont on est fiers.</div>
+                </div>
 
             </div>
-        </div>
+
+            <Footer />
+
+        </>
+
     )
 }
 
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,55 +1,77 @@
-import { useGSAP } from '@gsap/react'
-import ProjectCard from '../components/projects/ProjectCard'
-import gsap from 'gsap'
-import { ScrollTrigger } from 'gsap/all'
+import React, { useEffect } from 'react';
+import ProjectCard from '../components/projects/ProjectCard';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+import Footer from '../components/common/footer';
 
+gsap.registerPlugin(ScrollTrigger);
 
 const Projects = () => {
+    const projects = [
+        {
+            image1:
+                'https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960-1280x960.jpg',
+            image2:
+                'https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---Thumbnail-1280x960.jpg',
+        },
+        {
+            image1:
+                'https://k72.ca/uploads/caseStudies/OKA/OKA_thumbnail-1280x960.jpg',
+            image2:
+                'https://k72.ca/uploads/caseStudies/Opto/thumbnailimage_opto-1280x960.jpg',
+        },
+        {
+            image1:
+                'https://k72.ca/uploads/caseStudies/LAMAJEURE_-_Son_sur_mesure/chalaxeur-thumbnail_img-1280x960.jpg',
+            image2:
+                'https://k72.ca/uploads/caseStudies/SHELTON/thumbnailimage_shelton-1280x960.jpg',
+        },
+        {
+            image1:
+                'https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960-1280x960.jpg',
+            image2:
+                'https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---Thumbnail-1280x960.jpg',
+        },
+    ];
 
-    const projects = [{
-        image1: 'https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960-1280x960.jpg',
-        image2: 'https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---Thumbnail-1280x960.jpg'
-    }, {
-        image1: 'https://k72.ca/uploads/caseStudies/OKA/OKA_thumbnail-1280x960.jpg',
-        image2: 'https://k72.ca/uploads/caseStudies/Opto/thumbnailimage_opto-1280x960.jpg'
-    }, {
-        image1: 'https://k72.ca/uploads/caseStudies/LAMAJEURE_-_Son_sur_mesure/chalaxeur-thumbnail_img-1280x960.jpg',
-        image2: 'https://k72.ca/uploads/caseStudies/SHELTON/thumbnailimage_shelton-1280x960.jpg'
-    }]
+    useEffect(() => {
+        gsap.utils.toArray('.hero').forEach((hero) => {
+            gsap.set(hero, { transformOrigin: 'top', scaleY: 0 }); // start collapsed
 
-
-    gsap.registerPlugin(ScrollTrigger)
-
-    useGSAP(function () {
-        gsap.from('.hero', {
-            height: '100px',
-            stagger: {
-                amount: 0.4
-            },
-            scrollTrigger: {
-                trigger: '.lol',
-                start: 'top 100%',
-                end: 'top -150%',
-                scrub: true
-            }
-        })
-    })
+            gsap.to(hero, {
+                scaleY: 1, // grow to full height
+                scrollTrigger: {
+                    trigger: hero,
+                    start: 'top 80%',
+                    end: 'top 20%',
+                    scrub: true,
+                },
+                ease: 'none',
+            });
+        });
+    }, []);
 
     return (
-        <div className='lg:p-4 p-2 mb-[100vh]'>
-            <div className=' pt-[45vh]'>
-                <h2 className='font-[font2] lg:text-[9.5vw] text-7xl uppercase'>Projets</h2>
-            </div>
-            <div className='-lg:mt-20 lol'>
-                {projects.map(function (elem, idx) {
-                    return <div key={idx} className='hero w-full lg:h-[850px] mb-4 flex lg:flex-row flex-col lg:gap-4 gap-2'>
-                        <ProjectCard image1={elem.image1} image2={elem.image2} />
-                    </div>
-                })}
+        <section>
+            <div className="lg:p-4 p-2  border border-black">
+                <div className="pt-[45vh]">
+                    <h2 className="font-[font2] text-6xl sm:text-[9.5vw] lg:text-7xl uppercase">Projets</h2>
+                </div>
 
+                <div className="-lg:mt-20 lol">
+                    {projects.map((elem, idx) => (
+                        <div
+                            key={idx}
+                            className="hero w-full mb-4 flex lg:flex-row flex-col lg:gap-4 gap-2 overflow-hidden rounded-lg"
+                        >
+                            <ProjectCard image1={elem.image1} image2={elem.image2} />
+                        </div>
+                    ))}
+                </div>
             </div>
-        </div>
-    )
-}
+            <Footer />
+        </section>
+    );
+};
 
-export default Projects
+export default Projects;
